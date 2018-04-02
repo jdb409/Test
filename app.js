a1c0708c9ff7cae4e29365ae2deec3223faa0de2
brandjd <jonathan.brandwein@invesco.com>
2018-04-02 16:37:26 -0400

    asdfs

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
@@ -18,43 +16,14 @@ app.get('/', (req, res, next) => {
 });
 
 app.post('/', (req, res, next) => {
-    let list = {"messages": [
-        {
-          "items": [
+    let list =  {
+        "messages": [
             {
-              "description": "Item One Description",
-              "image": {
-                "url": "http://imageOneUrl.com"
-              },
-              "optionInfo": {
-                "key": "itemOne",
-                "synonyms": [
-                  "thing one",
-                  "object one"
-                ]
-              },
-              "title": "Item One"
-            },
-            {
-              "description": "Item Two Description",
-              "image": {
-                "url": "http://imageTwoUrl.com"
-              },
-              "optionInfo": {
-                "key": "itemTwo",
-                "synonyms": [
-                  "thing two",
-                  "object two"
-                ]
-              },
-              "title": "Item Two"
+            "speech": topic,
+            "type": 0,
+            "displayText":'asdfasdf'
             }
-          ],
-          "platform": "google",
-          "title": "Title",
-          "type": "list_card"
-        }
-      ]
+        ]
     }
     res.json(list);
 })
